4af5eac362d218ae9ee509b6562e637f8599e8e3
Anna Ridenour <annamridenour@gmail.com>
2019-09-18 16:47:30 -0700

    lab completed

--- __tests__/document-collection.test.js
+++ __tests__/document-collection.test.js
@@ -12,61 +12,75 @@ jest.mock('../lib/files.js', () => ({
   readdir: jest.fn(),
 }));
 
-
-
 // for setting up mock expectations
 const { readFile, writeFile, readdir } = require('../lib/files');
 
 describe('Document Collection', () => {
-  // it('example', () => {
-  //   //arrange
-  //   const source = './source.txt';
-  //   const dest = './dest.txt';
-  //   const fileContents = 'file contents';
-  //   const readPromise = Promise.resolve(fileContents);
-  //   readFile.mockReturnValueOnce(readPromise);
-
-  //   //act
-  //   return copyFile(source, dest)   //copy file uses readFile then writeFile
-  //     .then(() => {
-  //       //assert
-  //       expect(readFile.mock.calls[0][0]).toBe(source);  //[0][0] => first call, first argument
-  //       expect(writeFile.mock.calls[0][0]).toBe(dest);
-  //       expect(writeFile.mock.calls[0][1]).toBe(fileContents);
-        
-  //     });
-  // });
-
-  // it('error example', () => {
-  //   const error = 'read file error';
-  //   readFile.mockRejectedValueOnce(error);
-  //   expect.assertions(1); //this test can't pass just because the catch never fires - makes it so the catch has to fire or else the test fails
-
-  //   copyFile('badfile', 'dest')
-  //     .catch(err => {
-  //       expect(err).toBe(error);
-  //     });
-  // });
 
   it('writes an object to a new file', () => {
     const sample = {
       key: 'value',
       sick: true
     };
-   
+
     const writePromise = Promise.resolve(sample);
     writeFile.mockReturnValueOnce(writePromise);
     
     const dir = 'documents';
     const documents = new DocumentCollection(dir);
-    console.log(sample); 
     
     return documents.save(sample)
       .then(object => {
-        console.log('before expect');
         expect(path.dirname(writeFile.mock.calls[0][0])).toBe(dir);
         expect(writeFile.mock.calls[0][1]).toBe(JSON.stringify(sample));
         expect(object._id).toEqual(expect.any(String));
       });
-  }); 
+  });
+  
+  it('reads an object from a file', () => {
+    const sample = {
+      key: 'value',
+      sick: true,
+      _id: 'banana'
+    };
+
+    const readPromise = Promise.resolve(JSON.stringify(sample));
+    readFile.mockReturnValueOnce(readPromise);
+
+    const dir = 'documents';
+    const documents = new DocumentCollection(dir);
+
+    const id = sample._id;
+
+    return documents.get(id)
+      .then(object => {
+        expect(readFile.mock.calls[0][0]).toBe(`${dir}/${id}.json`);
+        expect(object._id).toBe('banana');
+      });
+  });
+
+  it('reads all objects from a directory', () => {
+    
+    const sample = {
+      key: 'value',
+      sick: true,
+      _id: 'banana'
+    };
+    
+    const readDirPromise = Promise.resolve(['banana.json']);
+    readdir.mockReturnValueOnce(readDirPromise);
+
+    const readPromise = Promise.resolve(JSON.stringify(sample));
+    readFile.mockReturnValueOnce(readPromise);
+    
+    const dir = 'documents';
+    const documents = new DocumentCollection(dir);
+
+    return documents.getAll()
+      .then(array => {
+        expect(readdir.mock.calls[0][0]).toBe(dir);
+        expect(readFile.mock.calls[0][0]).toBe(`${dir}/${sample._id}.json`);
+        expect(array[0]._id).toBe(sample._id);
+      });
+  });
 });
